01ca63c0780e9336c99bbb4911cb2767e5a613c4
Sama <samasky.rostami@gmail.com>
2023-09-03 19:01:55 +0330

    change send to broadcast and add MainRoomBroadcast

--- app/Http/Controllers/Api/v1/MessageController.php
+++ app/Http/Controllers/Api/v1/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Events\MainRoomBroadcast;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MessageStoreRequest;
 use App\Models\Message;
@@ -13,18 +14,20 @@ class MessageController extends Controller
     {
     }
 
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         return response()->json(Message::all());
     }
 
-    public function store(MessageStoreRequest $request)
+    public function broadcast(MessageStoreRequest $request): \Illuminate\Http\JsonResponse
     {
         $message = $request->validated()['message'];
         $newMessage = Message::query()->create([
             'user_id' => auth()->id(),
             'message' => $message
         ]);
+
+        broadcast(new MainRoomBroadcast($message));
 
         return response()->json($newMessage);
     }
